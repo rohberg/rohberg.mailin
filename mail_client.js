8ee0221c813c25d5c4152b196bb293a2458298e2
Katja Süss <k.suess@rohberg.ch>
2018-06-10 15:08:08 +0200

    post options
         json: true,
         body: newsletterObject,

--- mail_client.js
+++ mail_client.js
@@ -13,22 +13,40 @@ const inbox = {
 };
 
 notifier(inbox).on('mail', (mail) => {
-   const mailObject = {
-       from: mail.from[0].address,
-       name: mail.from[0].name,
-       date: mail.receivedDate,
-       subject: mail.subject ? mail.subject : 'No Subject',
-       message: mail.text && mail.text.replace(/\s/g,'').length > 0? mail.text : 'No Message body',
-       attachments: mail.attachments ? mail.attachments.map(value => value.fileName).join(' ') : 'None',
+   const newsletterObject = {
+       '@type': 'zhkathnewsletter',
+       'from': mail.from[0].address,
+       'date': mail.receivedDate,
+       'title': mail.subject ? mail.subject : 'No Subject',
+       'text': mail.text && mail.text.replace(/\s/g,'').length > 0? mail.text : 'No Message body',
    };
-   // request.post(process.env.WEBHOOK_TARGET).form(mailObject);
    console.info("*** New Email Received!");
-   console.info(mailObject['date'], '|', mailObject['subject'], '|', mailObject['from'], '|');
-   console.info(mailObject['message']);
-   console.info(mailObject);
-   // nur Newsletter, kein Spam
-   if (mailObject['from']==='newsletter@zh.kath.ch' || mailObject['from']==='newsletter@zhkath.ch') {
+   console.info(newsletterObject['date'], '|', newsletterObject['title']);
+   console.info(newsletterObject['text']);
+
+   const options = {
+     'uri': process.env.WEBHOOK_TARGET,
+     'headers': {
+       'User-Agent': 'mail-in client',
+        'Accept': 'application/json',
+        'Accept-Charset': 'utf-8',
+        'Content-Type': 'application/json',
+     },
+     json: true,
+     body: newsletterObject,
+   }
+
+   function callback(err, httpResponse, body){
+        console.debug(err);
+        console.debug('*** httpResponse');
+        console.debug(httpResponse && httpResponse.statusCode);
+        console.debug('');
+      }
+
+   request.post(options, callback).auth(process.env.PLONE_LOGIN, process.env.PLONE_PASSWORD);
+   // request.post(options, callback);
+   if (newsletterObject['from']==='newsletter@zh.kath.ch' || newsletterObject['from']==='newsletter@zhkath.ch') {
      // TODO: WEBHOOK_TARGET auf edgültige Adresse setzen um Plone Content zu erstellen
-     request.post(process.env.WEBHOOK_TARGET).form(mailObject);
+     // request.post(options, callback);
    }
 }).start();
